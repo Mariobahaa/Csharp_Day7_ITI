15c566450398139f6a19be66b95f37d5e05b149e
MarioBahaa <32718005+Mariobahaa@users.noreply.github.com>
2020-12-31 06:15:26 +0200

    Initial commit

---
+++ Problem1/Book.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problem1
+{
+    public delegate O GenericBDelegate<in T, out O>(T X);
+    public delegate string BDelegate(Book book);
+    public class Book
+    {
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+        public string[] Authors { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public decimal Price { get; set; }
+        public Book(string _ISBN, string _Title,
+        string[] _Authors, DateTime _PublicationDate,
+       decimal _Price)
+        {
+            ISBN = _ISBN;
+            Title = _Title;
+            Authors = _Authors;
+            PublicationDate = _PublicationDate;
+            Price = _Price;
+        }
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class BookFunctions
+    {
+        public static string GetTitle(Book B)
+        {
+            return B.Title;
+        }
+        public static string GetAuthors(Book B)
+        {
+            StringBuilder s = new StringBuilder("", 100);
+            for(int i=0;i<B.Authors?.Length;i++)
+            {
+                s.Append($"{B.Authors[i]} ,");
+            }
+            return s.ToString();
+        }
+        public static string GetPrice(Book B)
+        {
+            return B.Price.ToString();
+        }
+    }
+    public class LibraryEngine
+    {
+        public static void ProcessBooks(List<Book> bList
+       ,BDelegate fPtr)
+        {
+            foreach (Book B in bList)
+            {
+                Console.WriteLine(fPtr(B));
+            }
+        }
+    }
+}
---
+++ Problem1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //LibraryEngine libraryEngine = new LibraryEngine();
+            Book book = new Book("ISBN", "Hello C#", new string[1] { "Mario" }, DateTime.Now, 50);
+            BDelegate fptr = new BDelegate( BookFunctions.GetPrice);
+            List<Book> books = new List<Book>();
+            //books.Add(book);
+            LibraryEngine.ProcessBooks(books,fptr);
+        }
+    }
+}
